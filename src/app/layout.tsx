877fa2ebb465d4d186803c5aae525fb2a7af2846
Sergio Cerdá Hervás <sergio.cravas@gmail.com>
2024-04-04 19:43:42 +0200

    feat: Added color variables and installed scss

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "@/theme/globals.css";
+import { Outfit } from "next/font/google";
+import "@/theme/globals.scss";
 
-const inter = Inter({ subsets: ["latin"] });
+const outfit = Outfit({ subsets: ["latin"], weight: ['400', '500', '600', '700'] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={outfit.className}>{children}</body>
     </html>
   );
 }
